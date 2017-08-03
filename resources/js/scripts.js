fb4330804b0a055df3cea6ca95b23ba8f540a6c1
rweipi <rw.eipi@gmail.com>
2017-08-03 13:51:52 -0600

    dirty hack, have a better idea ?

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -45,5 +45,59 @@ $(document).ready(function () {
         }
     });
 
+    /* SCROLL BUTTONS */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-operations').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-operations').offset().top
+        }, 1000);
+    });
+
+    /* SCROLL NAV */
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* SCOLL ANIMATIONS*/
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated jello');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
 
 });
